a93621e0335a08b116bc21ffae0703bcaf00d4e0
James Galantino <james_galantino@aol.com>
2021-01-19 16:07:41 -0500

    Added pseudocode to index.html and main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+// eventListener, click event to hide inital start --> display first question
+
+// declare globals
+
+// var timer = 120 -seconds- (120000 -ms-)
+// timer--
+// var index = 0
+
+// start button evenListener to hide div element, also start timer
+// function showStuff(id, text, btn) {
+//     document.getElementById(id).style.display = 'block';
+//     // hide the lorem ipsum text
+//     document.getElementById(text).style.display = 'none';
+//     // hide the link
+//     btn.style.display = 'none';
+// }
+
+// array of object for question
+// quiz object to hold: questions, and possible answers (options)
+// var questions = [
+//     {
+//     question: "test",
+//     options: ["op1", "op2", "op3", "op4"],
+//     answer: "op2"
+//     },
+//     {
+//     question: "test1",
+//     options: ["op1", "op2", "op3", "op4"],
+//     answer: "op4"
+//     }
+//     ]
+
+// click event for each option
+// classes for correct, incorrect
+// link data attributes
+
+// if statement for user selected answer (option)
+// if correct, next question
+// if incorrect, next question, decrement time from timer (maybe 10-15 seconds)
+
+// if statement for stop timer. timer = 0 --> end quiz
